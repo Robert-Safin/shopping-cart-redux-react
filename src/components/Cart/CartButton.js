8bb237f3971327f033203fe365ad73df37e76132
Robert Safin <robertsafin98@gmail.com>
2023-04-05 21:10:24 +0800

    cart logic

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -3,9 +3,8 @@ import { uiActions } from '../../store/ui-slice';
 import { useSelector, useDispatch } from "react-redux";
 
 const CartButton = (props) => {
-
+  const cartQuantity = useSelector(state => state.cart.totalQuantity)
   const dispatch = useDispatch()
-
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle())
@@ -13,7 +12,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,47 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Yellow Banana",
+    description: "Its very yellow",
+  },
+  {
+    id: "p2",
+    price: 10,
+    title: "Blue Banana",
+    description: "Its very blue...?",
+  },
+  {
+    id: "p3",
+    price: 8,
+    title: "Orange Banana",
+    description: "Its very orange",
+  },
+  {
+    id: "p4",
+    price: 1,
+    title: "Rotten Banana",
+    description: "Its very rotten",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.description}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            id={product.id}
+          />
+        ))}
       </ul>
     </section>
   );
